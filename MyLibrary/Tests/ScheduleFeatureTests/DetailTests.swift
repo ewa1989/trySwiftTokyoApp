08e0d9f9f7151c8389ed6bd74fd1e808c14056d7
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2024-03-17 18:41:13 +0900

    add a test for view snsTapped on iOS and macOS

--- MyLibrary/Tests/ScheduleFeatureTests/DetailTests.swift
+++ MyLibrary/Tests/ScheduleFeatureTests/DetailTests.swift
@@ -8,14 +8,21 @@ import SharedModels
 final class DetailTests: XCTestCase {
 
   @MainActor
-  func test_justCreateTestStore() async {
+  func test_view_snsTapped_onIOSAndMacOS_presentSafari() async throws {
+    #if os(visionOS)
+      throw XCTSkip("this test is for iOS and macOS")
+    #endif
+
     let session: Session = .sessionWithDetailInfo
     let store = TestStore(initialState: ScheduleDetail.State(title: session.title,
                                                              description: session.description!,
                                                              speakers: session.speakers!)) {
       ScheduleDetail()
     }
-
-    XCTAssertNotNil(store)
+    
+    let url = session.speakers!.first!.links!.first!.url
+    await store.send(.view(.snsTapped(url))) {
+      $0.destination = .safari(.init(url: url))
+    }
   }
 }
