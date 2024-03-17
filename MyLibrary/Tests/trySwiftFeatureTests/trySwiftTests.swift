4e8d86cfe758ddc85a0bc76d0b1c861d788630c3
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2024-03-17 20:42:45 +0900

    add tests for view eventbriteTapped

--- MyLibrary/Tests/trySwiftFeatureTests/trySwiftTests.swift
+++ MyLibrary/Tests/trySwiftFeatureTests/trySwiftTests.swift
@@ -102,4 +102,42 @@ final class trySwiftTests: XCTestCase {
       $0.path.append(.acknowledgements(.init()))
     }
   }
+
+  @MainActor
+  func test_view_eventbriteTapped_onIOSAndMacOS_presentSafari() async throws {
+    #if os(visionOS)
+      throw XCTSkip("this test is for iOS and macOS")
+    #endif
+
+    let store = TestStore(initialState: TrySwift.State()) {
+      TrySwift()
+    }
+
+    await store.send(.view(.eventbriteTapped)) {
+      $0.destination = .safari(.init(url: URL(string: "https://www.eventbrite.com/e/try-swift-tokyo-2024-tickets-712565200697")!))
+    }
+  }
+
+  @MainActor
+  func test_view_eventbriteTapped_onVisionOS_openURL() async throws {
+    #if os(iOS) || os(macOS)
+      throw XCTSkip("this test is for visionOS")
+    #endif
+
+    let clock = TestClock()
+    let store = TestStore(initialState: TrySwift.State()) {
+      TrySwift()
+    } withDependencies: {
+      $0.openURL = OpenURLEffect {
+        XCTAssertEqual($0, URL(string: "https://www.eventbrite.com/e/try-swift-tokyo-2024-tickets-712565200697"))
+        return true
+      }
+      $0.continuousClock = clock
+    }
+
+    await store.send(.view(.eventbriteTapped))
+
+    // ensure to finish running all side effects
+    await clock.advance(by: .seconds(1))
+  }
 }
