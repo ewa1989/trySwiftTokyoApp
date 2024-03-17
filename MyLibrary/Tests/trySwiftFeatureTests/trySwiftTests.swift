80054947be47beded140d7ae1ba48b47063e82e4
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2024-03-17 20:35:49 +0900

    add tests for view codeOfConductTapped

--- MyLibrary/Tests/trySwiftFeatureTests/trySwiftTests.swift
+++ MyLibrary/Tests/trySwiftFeatureTests/trySwiftTests.swift
@@ -15,4 +15,42 @@ final class trySwiftTests: XCTestCase {
       $0.path.append(.organizers(.init()))
     }
   }
+
+  @MainActor
+  func test_view_codeOfConductTapped_onIOSAndMacOS_presentSafari() async throws {
+    #if os(visionOS)
+      throw XCTSkip("this test is for iOS and macOS")
+    #endif
+
+    let store = TestStore(initialState: TrySwift.State()) {
+      TrySwift()
+    }
+
+    await store.send(.view(.codeOfConductTapped)) {
+      $0.destination = .safari(.init(url: URL(string: "https://tryswift.jp/code-of-conduct")!))
+    }
+  }
+
+  @MainActor
+  func test_view_codeOfConductTapped_onVisionOS_openURL() async throws {
+    #if os(iOS) || os(macOS)
+      throw XCTSkip("this test is for visionOS")
+    #endif
+
+    let clock = TestClock()
+    let store = TestStore(initialState: TrySwift.State()) {
+      TrySwift()
+    } withDependencies: {
+      $0.openURL = OpenURLEffect {
+        XCTAssertEqual($0, URL(string: "https://tryswift.jp/code-of-conduct"))
+        return true
+      }
+      $0.continuousClock = clock
+    }
+
+    await store.send(.view(.codeOfConductTapped))
+
+    // ensure to finish running all side effects
+    await clock.advance(by: .seconds(1))
+  }
 }
