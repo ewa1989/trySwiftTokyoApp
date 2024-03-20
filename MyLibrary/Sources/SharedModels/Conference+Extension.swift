26506fbdf9dfa29accaca3ce262385496d6fe5c2
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2024-03-20 23:10:40 +0900

    change variable name of Conference, day to conference, as other place does

--- MyLibrary/Sources/SharedModels/Conference+Extension.swift
+++ MyLibrary/Sources/SharedModels/Conference+Extension.swift
@@ -1,7 +1,7 @@
 extension [Schedule] {
-  public func filtered(using favorites: Favorites, in day: Conference) -> Self {
+  public func filtered(using favorites: Favorites, in conference: Conference) -> Self {
     self
-      .map { Schedule(time: $0.time, sessions: $0.sessions.filter { favorites.isFavorited($0, in: day) }) }
+      .map { Schedule(time: $0.time, sessions: $0.sessions.filter { favorites.isFavorited($0, in: conference) }) }
       .filter { $0.sessions.count > 0 }
   }
 }
